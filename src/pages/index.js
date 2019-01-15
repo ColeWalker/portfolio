bbd9d27010351af34b2a0d0cec309f23b74a7698
ColeWalker <colegrwalker@gmail.com>
2019-01-15 12:41:12 -0500

    removed separate mobile site, adjusted layout accordingly

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,12 @@
 import React from 'react'
 import ParallaxWrapper from '../components/ParallaxWrapper'
 import SEO from '../components/seo'
-import MobileSite from '../components/mobilesite/MobileSite'
-import '../components/scss/mobileOrDesktop.scss'
 
 const IndexPage = () => (
   <>
   <SEO title={"Cole Portfolio"} />
   <div className={"desktopContainer"}>
     <ParallaxWrapper />
-  </div>
-  <div className={"mobileContainer"}>
-    <MobileSite> </MobileSite>
   </div>
   </>
 )
